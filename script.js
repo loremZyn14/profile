98cbcb38ac64b3b3661fb1a3e8605200a695b3dd
LoremZyn14 <yasuaimo@gmail.com>
2020-08-24 17:33:55 +0800

    add languages

--- script.js
+++ script.js
@@ -2,4 +2,25 @@
 new Vue({
     el: '#app',
     vuetify: new Vuetify(),
+    data() {
+        return {
+            others : [ 
+                   [
+                        { image : './assets/html.png', text: 'HTML 5'},
+                        { image : './assets/css.png', text: 'CSS 3'},
+                        { image : './assets/js.png', text: 'JS'},
+                        { image : './assets/jquery.png', text: 'JQUERY'},
+                    ],
+               
+
+                [
+                        { image : './assets/java.png', text: 'JAVA'},
+                        { image : './assets/angular.png',text:'Angular'},
+                        { image : './assets/nodejs.png' ,text:'NodeJs'},
+                        { image : './assets/mongodb.png' ,text:'MongoDb'},
+                    ]
+                 
+            ]
+        }
+    },
 })
